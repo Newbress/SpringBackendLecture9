c3cba6af00e0a9342797e9fe994bbe8f074e3b4c
me <ditod3847@gmail.com>
2025-12-31 18:46:07 +0900

    2일차

---
+++ src/main/java/org/example/Head01_JavaProgrammingStart/ContinueExample.java
@@ -0,0 +1,11 @@
+package org.example.Head01_JavaProgrammingStart;
+
+public class ContinueExample {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++)
+        {
+            if(i % 2 == 0) continue;
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/org/example/Head01_JavaProgrammingStart/ImprovedForLoop.java
@@ -0,0 +1,10 @@
+package org.example.Head01_JavaProgrammingStart;
+
+public class ImprovedForLoop {
+    public static void main(String[] args) {
+        int [] scores = {90, 80, 70, 60};
+        for(int score : scores) {
+            System.out.println(score);
+        }
+    }
+}
