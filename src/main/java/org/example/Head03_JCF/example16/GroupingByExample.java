1f83d4163453f9233bebe27a8f3e9c002be40478
apple <apple@appleui-MacBookAir.local>
2026-01-09 19:08:21 +0900

    7일차

---
+++ src/main/java/org/example/Head03_JCF/example16/GroupingByExample.java
@@ -0,0 +1,17 @@
+package org.example.Head03_JCF.example16;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupingByExample {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Kim", "Lee", "Park", "Kang" ,"Choi", "Cha");
+
+        Map<Character, List<String>> grouped =names.stream()
+                .collect(Collectors.groupingBy(name->name.charAt(0)));
+
+        System.out.println(grouped);
+    }
+}
